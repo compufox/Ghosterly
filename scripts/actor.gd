1a9eceb52a5a9c19b5ff5bd7d99e1da00ba8bdd1
Zac <thezacattacks@gmail.com>
2019-03-08 20:46:57 -0500

    changed player to area2d
    moved actor closer to the player in world scene

--- scripts/actor.gd
+++ scripts/actor.gd
@@ -10,4 +10,9 @@ func _ready():
 
 func _process(delta):
     if controlled and color.color != controlColor:
-        
+        color.color = controlColor
+    elif not controlled:
+        color.color = Color(1,1,1)
+
+func toggle_control():
+    controlled = not controlled
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,6 @@
-extends KinematicBody2D
+extends Area2D
 
+var entered_body = null
 
 func _ready():
     pass
@@ -9,14 +10,24 @@ func _process(delta):
     
     if Input.is_action_pressed("player_up"):
         vel.y = -1
-    
     if Input.is_action_pressed("player_down"):
         vel.y = 1
-    
     if Input.is_action_pressed("player_left"):
         vel.x = -1
-        
     if Input.is_action_pressed("player_right"):
         vel.x = 1
     
     position += vel
+    
+    if Input.is_action_just_pressed("action") and entered_body != null:
+        entered_body.get_ref().toggle_control()
+
+func _on_player_area_entered(obj):
+    if obj.is_in_group("movable"):
+        entered_body = weakref(obj)
+
+
+func _on_player_area_exited(obj):
+    if entered_body != null and entered_body.get_ref() == obj:
+        entered_body.get_ref().controlled = false
+        entered_body = null
